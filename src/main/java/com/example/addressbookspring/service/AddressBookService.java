27defa3928169ac11f9d52dd632bae3a7b319fd9
Rajnish20 <rajnishsahu95@gmail.com>
2021-07-28 16:09:09 +0530

    Add-ability to store person data in local list

--- src/main/java/com/example/addressbookspring/service/AddressBookService.java
+++ src/main/java/com/example/addressbookspring/service/AddressBookService.java
@@ -9,32 +9,36 @@ import java.util.List;
 
 @Service
 public class AddressBookService implements IAddressBookService{
+
+    private List<PersonData> personDataList = new ArrayList<>();
     @Override
     public List<PersonData> getPersonData() {
-        List<PersonData> personDataList = new ArrayList<>();
-        personDataList.add(new PersonData(1,new PersonDTO("Sneha","New Delhi")));
         return personDataList;
     }
 
     @Override
     public PersonData getPersonById(int personId) {
-        PersonData personData =new PersonData(personId,new PersonDTO("Kajal","New Delhi"));
-        return personData;
+        return this.personDataList.get(personId - 1);
     }
 
     @Override
     public PersonData createPerson(PersonDTO personDTO) {
         PersonData personData = new PersonData(1,personDTO);
+        this.personDataList.add(personData);
         return personData;
     }
 
     @Override
     public PersonData updatePersonData(PersonDTO personDTO, int personId) {
-        PersonData personData = new PersonData(personId, personDTO);
+        PersonData personData = this.getPersonById(personId);
+        personData.setName(personDTO.getName());
+        personData.setCity(personDTO.getCity());
+        personDataList.set(personId - 1, personData);
         return personData;
     }
 
     @Override
     public void deletePerson(int personId) {
+        personDataList.remove(personId - 1);
     }
 }
